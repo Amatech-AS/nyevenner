2f2d87f8de8c1db7aed35ca3d490ee3907c19d92
Asle Kambuås <asle@Asle-sin-MacBook-M1.local>
2025-11-24 21:26:47 +0100

    Fikset PostCSS config for Vercel build

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {}, /* Her er endringen! */
-    autoprefixer: {},
-  },
-};
-
-export default config;
